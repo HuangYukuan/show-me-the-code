511894f57ed1ef80f30f7cf4b14c371b51f142ee
huang <HYK@mail.dlut.edu.cn>
2016-08-01 09:15:53 +0800

    用了sqlite代替MySql，因为本机没有安装MySql

---
+++ 0001+0002/activation code.py
@@ -0,0 +1,15 @@
+import sqlite3,random
+def activation_generator():
+    return str(random.randrange(0,100000))
+
+if __name__=='__main__':
+    conn = sqlite3.connect('activation_code.db')
+    cursor = conn.cursor()
+    cursor.execute('create table activation_key (id vchar(10) primary key,code vchar(20))')
+
+    for i in range(200):
+        insert = "insert into activation_key (id,code) values (\'{0}\',\'{1}\')".format(str(i),activation_generator())
+        cursor.execute(insert)
+    cursor.close()
+    conn.commit()
+    conn.close()
