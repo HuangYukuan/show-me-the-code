ee5a36e0abb393998b73fc029e0a6f031034c322
huang <HYK@mail.dlut.edu.cn>
2016-08-09 13:50:34 +0800

    第六题完成

---
+++ 0006/0006.py
@@ -0,0 +1,11 @@
+import re
+from collections import Counter
+with open('test.txt','r') as f:
+    s=f.read()
+    f.close()
+filter_word=('am','is','are','was','be','s','re','and','at','on','in','to','m','has','have','with','as','a','an','the')
+matched=re.findall(r'[a-zA-Z]+-?[a-zA-Z]*',s)
+counter=Counter(matched)
+for i in filter_word:
+    del counter[i]
+print(counter.most_common(1)[0][0])
