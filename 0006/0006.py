d415b08ef2ef67ac9d8eff2e06aca60cf3ceff3e
huang <HYK@mail.dlut.edu.cn>
2016-08-09 13:52:39 +0800

    修改

--- 0006/0006.py
+++ 0006/0006.py
@@ -3,7 +3,7 @@ from collections import Counter
 with open('test.txt','r') as f:
     s=f.read()
     f.close()
-filter_word=('am','is','are','was','be','s','re','and','at','on','in','to','m','has','have','with','as','a','an','the')
+filter_word=('am','is','are','was','be','s','re','and','at','on','in','to','m','has','have','with','as','a','an','the','of')
 matched=re.findall(r'[a-zA-Z]+-?[a-zA-Z]*',s)
 counter=Counter(matched)
 for i in filter_word:
